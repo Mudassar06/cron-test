a6517fc6688056be682534e32b179e1661fa3f95
Mudassar <mudassar0606@gmail.com>
2024-02-26 22:19:40 +0530

    added supabase

--- app/api/getdate/route.js
+++ app/api/getdate/route.js
@@ -1,12 +1,27 @@
+import { createClient } from "@supabase/supabase-js";
+
 export async function GET(request){
-    const currentdate = new Date(); 
     
+    const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    )
+
+    const random = parseInt(Math.random()*100);    
+    const currentdate = new Date(); 
+
     var datetime = "Last Sync: " + currentdate.getDate() + "/"
                 + (currentdate.getMonth()+1)  + "/" 
                 + currentdate.getFullYear() + " @ "  
                 + currentdate.getHours() + ":"  
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
+    
+    const { data } = await supabase
+    .from('cron-test')
+    .update({ temp: random})
+    .eq('id', 1)
+    console.log(data)
     
     return new Response(datetime)
 }
